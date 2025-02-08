b2e1a9aaaa0b52428fdc1b366a58c2e1051b610e
kyle-woodward <kw.geospatial@gmail.com>
2025-02-08 13:35:47 -0500

    Black formatting + README sprucing

--- crop_mle/load_data.py
+++ crop_mle/load_data.py
@@ -5,49 +5,62 @@ import logging, os
 from datetime import datetime
 
 # Set up logging
-log_dir = os.path.join(os.path.dirname(__file__), '..', '.log')
+log_dir = os.path.join(os.path.dirname(__file__), "..", ".log")
 os.makedirs(log_dir, exist_ok=True)
-log_filename = datetime.now().strftime('logfile_%Y%m%d_%H%M%S.log')
+log_filename = datetime.now().strftime("logfile_%Y%m%d_%H%M%S.log")
 log_filepath = os.path.join(log_dir, log_filename)
 
-logging.basicConfig(filename=log_filepath, level=logging.INFO, 
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename=log_filepath,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
-def load_gt(gt_path:str) -> gpd.GeoDataFrame:
-    """"
+
+def load_gt(gt_path: str) -> gpd.GeoDataFrame:
+    """ "
     Load ground truth vector data from file path.
-    
+
     Args:
         gt_path (str): Path to the ground truth vector data file.
-    
+
     Returns:
         gpd.GeoDataFrame: Ground truth vector data.
     """
     return gpd.read_file(gt_path)
 
-def schema_check(fields:gpd.GeoDataFrame,
-                 label_col: str, 
-                 label_map:CropTypeDictionary) -> gpd.GeoDataFrame:
+
+def schema_check(
+    fields: gpd.GeoDataFrame, label_col: str, label_map: CropTypeDictionary
+) -> gpd.GeoDataFrame:
     """
-    Conducts a schema check of the fields data to ensure all crop type values in `label_col` have a matching label `label_map`. 
+    Conducts a schema check of the fields data to ensure all crop type values in `label_col` have a matching label `label_map`.
         Non-conforming records are removed from the GeoDataFrame and are logged.
 
     Args:
         fields (gpd.GeoDataFrame): DataFrame containing field data.
         label_col (str): Column name for crop type values.
         label_map (CropTypeDictionary): Dictionary mapping crop types to labels.
-    
+
     Returns:
         gpd.GeoDataFrame: DataFrame with only fields that have crop types in the label_map.
     """
     label_map_dict = asdict(label_map)
-    crop_dict = label_map_dict['crop_dict']
-    _fields = fields.copy() # copy so we can use original to look at non-conforming fields for info message
-    _fields[label_col] = fields[label_col].apply(lambda x: next((k for k, v in crop_dict.items() if x in v), None))
+    crop_dict = label_map_dict["crop_dict"]
+    _fields = (
+        fields.copy()
+    )  # copy so we can use original to look at non-conforming fields for info message
+    _fields[label_col] = fields[label_col].apply(
+        lambda x: next((k for k, v in crop_dict.items() if x in v), None)
+    )
     non_conforming = _fields[_fields[label_col].isna()]
     if non_conforming.shape[0] > 0:
-        logging.info(f"Fields with crop types not in label_map: {non_conforming.shape[0]}")
-        logging.info(f"Non-conforming fields: {non_conforming['field_id'].values}\n {fields.loc[non_conforming.index][label_col]}")
+        logging.info(
+            f"Fields with crop types not in label_map: {non_conforming.shape[0]}"
+        )
+        logging.info(
+            f"Non-conforming fields: {non_conforming['field_id'].values}\n {fields.loc[non_conforming.index][label_col]}"
+        )
         keep = _fields.dropna(subset=[label_col])
         conforming = fields.loc[keep.index]
     else:
--- crop_mle/main.py
+++ crop_mle/main.py
@@ -1,6 +1,13 @@
 from load_data import load_gt, schema_check
 from process import aggregate_predictions
-from evaluate import standardize_labels, record_count, agreement, average_confidence, cm_f1, plot_confusion_matrix
+from evaluate import (
+    standardize_labels,
+    record_count,
+    agreement,
+    average_confidence,
+    cm_f1,
+    plot_confusion_matrix,
+)
 from dataclasses import asdict
 from _types import CropTypeDictionary
 import pandas as pd
@@ -10,13 +17,18 @@ from datetime import datetime
 import argparse
 
 # Set up logging
-log_dir = os.path.join(os.path.dirname(__file__), '..', '.log')
+log_dir = os.path.join(os.path.dirname(__file__), "..", ".log")
 os.makedirs(log_dir, exist_ok=True)
-log_filename = datetime.now().strftime('logfile_%Y%m%d_%H%M%S.log')
+log_filename = datetime.now().strftime("logfile_%Y%m%d_%H%M%S.log")
 log_filepath = os.path.join(log_dir, log_filename)
 
-logging.basicConfig(filename=log_filepath, level=logging.INFO, 
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename=log_filepath,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+
 def main():
     """
     Main function to evaluate crop model predictions.
@@ -24,81 +36,94 @@ def main():
     Example usage:
         python main.py --gt /path/to/ground_truth.gpkg --raster /path/to/prediction_raster.tif
     """
-    
+
     # Set up argument parser
-    parser = argparse.ArgumentParser(description='Crop Model Performance Analysis.',
-                                     epilog='Example usage: python main.py --gt /path/to/ground_truth.gpkg --raster /path/to/prediction_raster.tif',
-                                     formatter_class=argparse.RawTextHelpFormatter
-                                     )
-    parser.add_argument('--gt', type=str, required=True, help='Path to the ground truth vector file')
-    parser.add_argument('--raster', type=str, required=True, help='Path to the prediction raster data file')
-    parser.add_argument('--label_field', type=str, default='normalized_label', help='Field name in the ground truth data containing crop type labels')
+    parser = argparse.ArgumentParser(
+        description="Crop Model Performance Analysis.",
+        epilog="Example usage: python main.py --gt /path/to/ground_truth.gpkg --raster /path/to/prediction_raster.tif",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "--gt", type=str, required=True, help="Path to the ground truth vector file"
+    )
+    parser.add_argument(
+        "--raster",
+        type=str,
+        required=True,
+        help="Path to the prediction raster data file",
+    )
+    parser.add_argument(
+        "--label_field",
+        type=str,
+        default="normalized_label",
+        help="Field name in the ground truth data containing crop type labels",
+    )
     args = parser.parse_args()
 
     # load data, data dictionary, and conduct schema check on fields
     gt_path = args.gt
     raster_path = args.raster
     label_field = args.label_field
-    
-    print(f'Starting Process...check progress at {log_filepath}')
-    
+
+    print(f"Starting Process...check progress at {log_filepath}")
+
     # we'll store results in a repo-level directory
-    results_dir = os.path.join(os.path.dirname(__file__), '..', 'results')
+    results_dir = os.path.join(os.path.dirname(__file__), "..", "results")
     os.makedirs(results_dir, exist_ok=True)
-    
+
     # initialize cropt tpye dictionary and access labels
     labels_dict = CropTypeDictionary()
-    label_list = list(asdict(labels_dict)['crop_dict'].keys())
+    label_list = list(asdict(labels_dict)["crop_dict"].keys())
 
     # load ground truth data and conduct schema check
-    fields = load_gt(gt_path)  
+    fields = load_gt(gt_path)
     fields = schema_check(fields, label_field, labels_dict)
 
     # aggregate model predictions to fields
     preds = aggregate_predictions(raster_path, fields)
 
     # merge ground truth and model predictions dataframes and save out to csv (useful for debugging)
-    merged_df = fields.merge(preds, on="field_id", how="left", suffixes=('_field', '_pred'))
-    merged_df.dropna(inplace=True) # drop fields with no predictions (i.e. no pixels in field)
-    merged_df.to_csv(results_dir+'/merged_df.csv', index=False)
+    merged_df = fields.merge(
+        preds, on="field_id", how="left", suffixes=("_field", "_pred")
+    )
+    merged_df.dropna(
+        inplace=True
+    )  # drop fields with no predictions (i.e. no pixels in field)
+    merged_df.to_csv(results_dir + "/merged_df.csv", index=False)
 
-    merged_df = standardize_labels(merged_df,
-                                        label_field, 
-                                        "predicted_int",
-                                        labels_dict
-                                        )
+    merged_df = standardize_labels(merged_df, label_field, "predicted_int", labels_dict)
     # compute counts by crop type and agreement between ground truth and model predictions
-    counts_df = record_count(merged_df, 'gt_label')
-    agreement_df = agreement(merged_df, 'gt_label', 'pred_label')
-    
+    counts_df = record_count(merged_df, "gt_label")
+    agreement_df = agreement(merged_df, "gt_label", "pred_label")
+
     # get average confidence by crop type for model predictions
-    avg_conf_df = average_confidence(merged_df, 'pred_label', 'confidence')
-    
+    avg_conf_df = average_confidence(merged_df, "pred_label", "confidence")
+
     # compute confusion matrix and F1 scores
-    cm, f1_scores_df = cm_f1(merged_df, 
-                                   "gt_label", 
-                                   "pred_label",
-                                   labels_dict
-                                   )
+    cm, f1_scores_df = cm_f1(merged_df, "gt_label", "pred_label", labels_dict)
     # Save confusion matrix csv/png
-    plot_confusion_matrix(cm, 
-                          label_list, 
-                          output_path=results_dir+'/confusion_matrix.png')
+    plot_confusion_matrix(
+        cm, label_list, output_path=results_dir + "/confusion_matrix.png"
+    )
     cm_df = pd.DataFrame(cm)
-    cm_df.to_csv(os.path.join(results_dir, 'confusion_matrix.csv'), index=True)
-    
+    cm_df.to_csv(os.path.join(results_dir, "confusion_matrix.csv"), index=True)
+
     # Merge crop counts, agreement and f1_scores for final .csv
-    ct_agree = counts_df.merge(agreement_df, on='gt_label', how='left')
-    f1_conf = f1_scores_df.merge(avg_conf_df, left_on='Crop',right_on='pred_label',how="left")
-    final_df = f1_conf.merge(ct_agree, left_on='Crop', right_on='gt_label', how='left')
-    
+    ct_agree = counts_df.merge(agreement_df, on="gt_label", how="left")
+    f1_conf = f1_scores_df.merge(
+        avg_conf_df, left_on="Crop", right_on="pred_label", how="left"
+    )
+    final_df = f1_conf.merge(ct_agree, left_on="Crop", right_on="gt_label", how="left")
+
     # drop any crop types with no gt_label (i.e. no instances in the ground truth)
-    final_df.dropna(subset=['gt_label'], inplace=True) 
-    
+    final_df.dropna(subset=["gt_label"], inplace=True)
+
     # clean-up duplicate columns and write out final result
-    final_df.drop(columns=['gt_label','pred_label'], inplace=True)
-    final_df.to_csv(os.path.join(results_dir, 'final_results.csv'), index=False)
-    logging.info('Done')
+    final_df.drop(columns=["gt_label", "pred_label"], inplace=True)
+    final_df.to_csv(os.path.join(results_dir, "final_results.csv"), index=False)
+    logging.info("Done")
+
+
 if __name__ == "__main__":
     start_time = time.time()
     main()
